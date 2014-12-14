1d7c1b2fc9c3710700156e5ba6cc64951f66a63e
Alessio Morale <alessiomorale@gmail.com>
2014-12-14 18:15:52 +0100

    REVONANO - Change target to 411, fix ram and flash extents, raise clock to 96MHz (cannot do 100 because of USB) , cleanup spl

--- flight/targets/boards/revonano/board-info.mk
+++ flight/targets/boards/revonano/board-info.mk
@@ -4,8 +4,8 @@ BOOTLOADER_VERSION  := 0x06
 HW_TYPE             := 0x00
 
 MCU                 := cortex-m4
-CHIP                := STM32F401CC
-CHIPFAMILY			:= STM32F401xx
+CHIP                := STM32F411CE
+CHIPFAMILY			:= STM32F411xx
 BOARD               := STM32F4xx_RN
 MODEL               := HD
 MODEL_SUFFIX        := 
@@ -23,10 +23,7 @@ OPENOCD_CONFIG      := stm32f4xx.stlink.cfg
 # 5		0x0802 0000	128k	FW     --- Proto
 # 6		0x0804 0000	128k	FW
 # 7 	0x0806 0000	128k	FW
-# 8 	0x0808 0000	128k	Unused
-# 9		0x080A 0000	128k	Unused
-# 10	0x080C 0000	128k	Unused						..
-# 11	0x080E 0000	128k   	Unused
+
 
 # Note: These must match the values in link_$(BOARD)_memory.ld
 BL_BANK_BASE        := 0x08000000  # Start of bootloader flash
@@ -40,9 +37,9 @@ EE_BANK_SIZE        := 0x00008000  # Size of EEPROM storage area
 # Leave the remaining 64KB sectors for other uses
 
 FW_BANK_BASE        := 0x08010000  # Start of firmware flash
-FW_BANK_SIZE        := 0x00060000  # Should include FW_DESC_SIZE
+FW_BANK_SIZE        := 0x00070000  # Should include FW_DESC_SIZE
 
 FW_DESC_SIZE        := 0x00000064
 
 OSCILLATOR_FREQ     :=   8000000
-SYSCLK_FREQ         :=  84000000
+SYSCLK_FREQ         :=  96000000

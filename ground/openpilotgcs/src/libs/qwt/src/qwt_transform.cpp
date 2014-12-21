59570c86b2977c3c14d6d67906446305f667880d
Stefan Karlsson <stefan.karlsson@gmail.com>
2014-12-21 11:57:28 +0100

    Remove QT_STATIC_CONST and QT_STATIC_CONST_IMPL

--- ground/openpilotgcs/src/libs/qwt/src/qwt_transform.cpp
+++ ground/openpilotgcs/src/libs/qwt/src/qwt_transform.cpp
@@ -25,10 +25,10 @@ const double QwtLogTransform::LogMax = 1.0e150;
 #else
 
 //! Smallest allowed value for logarithmic scales: 1.0e-150
-QT_STATIC_CONST_IMPL double QwtLogTransform::LogMin = 1.0e-150;
+const double QwtLogTransform::LogMin = 1.0e-150;
 
 //! Largest allowed value for logarithmic scales: 1.0e150
-QT_STATIC_CONST_IMPL double QwtLogTransform::LogMax = 1.0e150;
+const double QwtLogTransform::LogMax = 1.0e150;
 
 #endif
 

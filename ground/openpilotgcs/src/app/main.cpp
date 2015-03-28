2eabe05733d04538161b3b02a0c332c0fc9754a8
Philippe Renon <op.filnet@yahoo.fr>
2015-03-28 17:23:04 +0100

    OP-1518 simplified GCS system initialization

--- ground/openpilotgcs/src/app/main.cpp
+++ ground/openpilotgcs/src/app/main.cpp
@@ -256,17 +256,17 @@ void systemInit()
     getrlimit(RLIMIT_NOFILE, &rl);
     rl.rlim_cur = rl.rlim_max;
     setrlimit(RLIMIT_NOFILE, &rl);
+#endif
 
-    // Force "basic" render loop on Mac
-    // Only Mac uses "threaded" by default and that mode currently does not work well with OSGViewport
-    qputenv("QSG_RENDER_LOOP", "basic");
-#endif
-#ifdef Q_OS_LINUX
     QApplication::setAttribute(Qt::AA_X11InitThreads, true);
-#endif
+
     // protect QQuickWidget from native widgets like GLC ModelView
     // TODO revisit this... might cause issues with gstreamer
     QApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings, true);
+
+    // Force "basic" render loop
+    // Only Mac uses "threaded" by default and that mode currently does not work well with OSGViewport
+    qputenv("QSG_RENDER_LOOP", "basic");
 }
 
 static QTextStream *logStream;

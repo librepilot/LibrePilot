2ce0d5f3785500c8d32fb70707877f681b3879ae
m_thread <fredrik@arvidssons.org>
2015-04-27 01:00:14 +0200

    OP-1837 Added SRXL support to nano.

--- flight/targets/boards/revonano/board_hw_defs.c
+++ flight/targets/boards/revonano/board_hw_defs.c
@@ -348,6 +348,7 @@ static const struct pios_dsm_cfg pios_dsm_main_cfg = {
 
 #endif /* PIOS_INCLUDE_DSM */
 
+
 #include <pios_sbus_priv.h>
 #if defined(PIOS_INCLUDE_SBUS)
 /*
@@ -534,6 +535,54 @@ static const struct pios_dsm_cfg pios_dsm_flexi_cfg = {
 
 #endif /* PIOS_INCLUDE_DSM */
 
+#if defined(PIOS_INCLUDE_SRXL)
+/*
+ * SRXL USART
+ */
+#include <pios_srxl_priv.h>
+
+static const struct pios_usart_cfg pios_usart_srxl_flexi_cfg = {
+    .regs  = FLEXI_USART_REGS,
+    .remap = FLEXI_USART_REMAP,
+    .init  = {
+        .USART_BaudRate   = 115200,
+        .USART_WordLength = USART_WordLength_8b,
+        .USART_Parity     = USART_Parity_No,
+        .USART_StopBits   = USART_StopBits_1,
+        .USART_HardwareFlowControl             = USART_HardwareFlowControl_None,
+        .USART_Mode                            = USART_Mode_Rx,
+    },
+    .irq                                       = {
+        .init                                  = {
+            .NVIC_IRQChannel    = FLEXI_USART_IRQ,
+            .NVIC_IRQChannelPreemptionPriority = PIOS_IRQ_PRIO_HIGH,
+            .NVIC_IRQChannelSubPriority        = 0,
+            .NVIC_IRQChannelCmd = ENABLE,
+        },
+    },
+    .rx                                        = {
+        .gpio = FLEXI_USART_RX_GPIO,
+        .init = {
+            .GPIO_Pin   = FLEXI_USART_RX_PIN,
+            .GPIO_Speed = GPIO_Speed_2MHz,
+            .GPIO_Mode  = GPIO_Mode_AF,
+            .GPIO_OType = GPIO_OType_PP,
+            .GPIO_PuPd  = GPIO_PuPd_UP
+        },
+    },
+    .tx                                        = {
+        .gpio = FLEXI_USART_TX_GPIO,
+        .init = {
+            .GPIO_Pin   = FLEXI_USART_TX_PIN,
+            .GPIO_Speed = GPIO_Speed_2MHz,
+            .GPIO_Mode  = GPIO_Mode_OUT,
+            .GPIO_OType = GPIO_OType_PP,
+            .GPIO_PuPd  = GPIO_PuPd_UP
+        },
+    },
+};
+
+#endif /* PIOS_INCLUDE_SRXL */
 /*
  * HK OSD
  */

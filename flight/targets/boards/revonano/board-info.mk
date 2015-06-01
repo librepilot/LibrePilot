1985f19fc0dd98ce18dba8b5c9d666714f18af62
Alessio Morale <alessiomorale@gmail.com>
2015-06-01 18:57:29 +0200

    REVONANO - workaround the issue with usart baud rate rounding changing slightly the Xtal frequency. Remove the former sbus rate issue workaround

--- flight/targets/boards/revonano/board-info.mk
+++ flight/targets/boards/revonano/board-info.mk
@@ -40,6 +40,7 @@ FW_BANK_BASE        := 0x08010000  # Start of firmware flash
 FW_BANK_SIZE        := 0x00070000  # Should include FW_DESC_SIZE
 
 FW_DESC_SIZE        := 0x00000064
-
-OSCILLATOR_FREQ     :=   8000000
+# Quartz frequency is actually 8000000. 
+# The following value fixes several rounding errors on uart baud rates calculations
+OSCILLATOR_FREQ     :=   7999999
 SYSCLK_FREQ         :=  96000000

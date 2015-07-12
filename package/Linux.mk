4c7e52db24ed51b485eeff69aae238a83c6420ae
Paul Jewell <paul@teulu.org>
2015-07-12 18:03:19 +0100

    Removed udevdir from Linux.mk

--- package/Linux.mk
+++ package/Linux.mk
@@ -98,9 +98,6 @@ install:
 	$(V1) $(MKDIR) -p $(DESTDIR)$(datadir)
 	$(V1) $(MKDIR) -p $(DESTDIR)$(datadir)/applications
 	$(V1) $(MKDIR) -p $(DESTDIR)$(datadir)/pixmaps
-# Following line creates udev directory - however udevdir is not previously defined, nor is anything
-# installed to this location in the rest of this recipe
-#	$(V1) $(MKDIR)  $(DESTDIR)$(udevdir)
 	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/bin/$(GCS_SMALL_NAME) $(DESTDIR)$(bindir)
 	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/lib/$(GCS_SMALL_NAME) $(DESTDIR)$(libdir)
 	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/share/$(GCS_SMALL_NAME) $(DESTDIR)$(datadir)

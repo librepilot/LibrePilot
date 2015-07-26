2eb9a31d6f30b5c762f797dddeb4bfc81087c406
James Duley <jagduley@gmail.com>
2015-07-26 21:30:41 +0100

    LP-61 addtional make config options: added libbasename & GCS_LIBRARY_BASENAME
    Change-Id: Ied2f50b3f87d40ef7fb0bbad0fff4cd0fd9bb8a9

--- package/Linux.mk
+++ package/Linux.mk
@@ -85,7 +85,8 @@ enable-udev-rules = no
 
 prefix        = /usr/local
 bindir        = $(prefix)/bin
-libdir        = $(prefix)/lib
+libbasename   = lib
+libdir        = $(prefix)/$(libbasename)
 datadir       = $(prefix)/share
 udevrulesdir  = /etc/udev/rules.d
 
@@ -102,7 +103,7 @@ install:
 	$(V1) $(MKDIR) -p $(DESTDIR)$(datadir)/applications
 	$(V1) $(MKDIR) -p $(DESTDIR)$(datadir)/pixmaps
 	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/bin/$(GCS_SMALL_NAME) $(DESTDIR)$(bindir)
-	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/lib/$(GCS_SMALL_NAME) $(DESTDIR)$(libdir)
+	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/$(libbasename)/$(GCS_SMALL_NAME) $(DESTDIR)$(libdir)
 	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/share/$(GCS_SMALL_NAME) $(DESTDIR)$(datadir)
 	$(V1) $(INSTALL) -T $(ROOT_DIR)/package/linux/gcs.desktop $(DESTDIR)$(datadir)/applications/$(ORG_SMALL_NAME).desktop
 	$(V1) sed -i -e 's/gcs/$(GCS_SMALL_NAME)/g;s/GCS/$(GCS_BIG_NAME)/g;s/org/$(ORG_SMALL_NAME)/g;s/ORG/$(ORG_BIG_NAME)/g' $(DESTDIR)$(datadir)/applications/$(ORG_SMALL_NAME).desktop

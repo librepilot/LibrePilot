aa0405001af419c0c8442b4bbe13440bfe62872f
Roy Bekken <Roy.Bekken@gmail.com>
2015-07-29 16:08:50 +0200

    LP-1 - Code Rebranding
    Use a relevant icon for linux desktop.

--- package/Linux.mk
+++ package/Linux.mk
@@ -78,7 +78,7 @@ $(DEB_PACKAGE_DIR): $(DEB_ORIG_SRC) debian | $(PACKAGE_DIR)
 endif # Debian based distro?
 ##############################
 #
-# Install OpenPilot
+# Install Linux Target
 #
 ##############################
 enable-udev-rules ?= no
@@ -104,9 +104,11 @@ install:
 	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/lib/$(GCS_SMALL_NAME) $(DESTDIR)$(libdir)
 	$(V1) $(INSTALL) $(BUILD_DIR)/$(GCS_SMALL_NAME)_$(GCS_BUILD_CONF)/share/$(GCS_SMALL_NAME) $(DESTDIR)$(datadir)
 	$(V1) $(INSTALL) -T $(ROOT_DIR)/package/linux/gcs.desktop $(DESTDIR)$(datadir)/applications/$(ORG_SMALL_NAME).desktop
-	$(V1) sed -i -e 's/gcs/$(GCS_SMALL_NAME)/g;s/GCS/$(GCS_BIG_NAME)/g;s/org/$(ORG_SMALL_NAME)/g;s/ORG/$(ORG_BIG_NAME)/g' $(DESTDIR)$(datadir)/applications/$(ORG_SMALL_NAME).desktop
+	$(V1) $(INSTALL) -T $(ROOT_DIR)/ground/openpilotgcs/src/plugins/coreplugin/images/$(ORG_SMALL_NAME)_logo_128.png \
+		$(DESTDIR)$(datadir)/pixmaps/$(ORG_SMALL_NAME).png
 
-	$(V1) $(INSTALL) -T $(ROOT_DIR)/package/linux/openpilot.png $(DESTDIR)$(datadir)/pixmaps/$(ORG_SMALL_NAME).png
+	$(V1) sed -i -e 's/gcs/$(GCS_SMALL_NAME)/g;s/GCS/$(GCS_BIG_NAME)/g;s/org/$(ORG_SMALL_NAME)/g;s/ORG/$(ORG_BIG_NAME)/g' \
+		$(DESTDIR)$(datadir)/applications/$(ORG_SMALL_NAME).desktop
 
 ifneq ($(enable-udev-rules), no)
 	$(V1) $(MKDIR) -p $(DESTDIR)$(udevrulesdir)

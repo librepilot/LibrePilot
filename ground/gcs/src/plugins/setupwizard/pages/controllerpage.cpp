66aab9553e798c42f881d9d5744f978028a0ec9f
Laurent Lalanne <f5soh@free.fr>
2015-08-28 18:56:10 +0200

    LP-106 Add board picture to wizard controller page - Resize connection diagram button

--- ground/gcs/src/plugins/setupwizard/pages/controllerpage.cpp
+++ ground/gcs/src/plugins/setupwizard/pages/controllerpage.cpp
@@ -199,6 +199,35 @@ void ControllerPage::connectionStatusChanged()
 
         SetupWizard::CONTROLLER_TYPE type = getControllerType();
         setControllerType(type);
+        QPixmap boardPic;
+        QSize picSize = QSize(250,250);
+
+        switch (type) {
+        case SetupWizard::CONTROLLER_CC:
+            boardPic.load(":/configgadget/images/coptercontrol.svg");
+            ui->boardImg->setPixmap(boardPic.scaled(picSize, Qt::KeepAspectRatio));
+            break;
+
+        case SetupWizard::CONTROLLER_CC3D:
+            boardPic.load(":/configgadget/images/cc3d_top.png");
+            ui->boardImg->setPixmap(boardPic.scaled(picSize, Qt::KeepAspectRatio));
+            break;
+
+        case SetupWizard::CONTROLLER_REVO:
+        case SetupWizard::CONTROLLER_DISCOVERYF4:
+            boardPic.load(":/configgadget/images/revolution_top.png");
+            ui->boardImg->setPixmap(boardPic.scaled(picSize, Qt::KeepAspectRatio));
+            break;
+
+        case SetupWizard::CONTROLLER_NANO:
+            boardPic.load(":/configgadget/images/nano_top.png");
+            ui->boardImg->setPixmap(boardPic.scaled(picSize, Qt::KeepAspectRatio));
+            break;
+
+        default:
+            ui->boardImg->setPixmap(QPixmap());
+            break;
+        }
         qDebug() << "Connection status changed: Connected, controller type: " << getControllerType();
     } else {
         ui->deviceCombo->setEnabled(true);
@@ -206,6 +235,7 @@ void ControllerPage::connectionStatusChanged()
         ui->boardTypeCombo->setEnabled(false);
         ui->boardTypeCombo->model()->setData(ui->boardTypeCombo->model()->index(0, 0), QVariant(0), Qt::UserRole - 1);
         setControllerType(SetupWizard::CONTROLLER_UNKNOWN);
+        ui->boardImg->setPixmap(QPixmap());
         qDebug() << "Connection status changed: Disconnected";
     }
     emit completeChanged();

a110eeda21030c77dcf8480ba3154b5e288f0e3b
Cliff Geerdes <cliffsjunk@att.net>
2015-09-16 01:33:32 -0400

    LP-73 Remove changes to CopterControl

--- flight/targets/boards/coptercontrol/firmware/pios_board.c
+++ flight/targets/boards/coptercontrol/firmware/pios_board.c
@@ -36,7 +36,6 @@
 #include <taskinfo.h>
 #include <sanitycheck.h>
 #include <actuatorsettings.h>
-#include <auxmagsettings.h>
 
 #ifdef PIOS_INCLUDE_INSTRUMENTATION
 #include <pios_instrumentation.h>
@@ -54,24 +53,6 @@
  * NOTE: THIS IS THE ONLY PLACE THAT SHOULD EVER INCLUDE THIS FILE
  */
 #include "../board_hw_defs.c"
-
-#if defined(PIOS_INCLUDE_HMC5X83)
-#include "pios_hmc5x83.h"
-pios_hmc5x83_dev_t external_mag = 0;
-
-static const struct pios_hmc5x83_cfg pios_hmc5x83_external_cfg = {
-#ifdef PIOS_HMC5X83_HAS_GPIOS
-    .exti_cfg    = NULL,
-#endif
-    .M_ODR       = PIOS_HMC5x83_ODR_75, // if you change this for auxmag, change AUX_MAG_SKIP in sensors.c
-    .Meas_Conf   = PIOS_HMC5x83_MEASCONF_NORMAL,
-    .Gain        = PIOS_HMC5x83_GAIN_1_9,
-    .Mode        = PIOS_HMC5x83_MODE_CONTINUOUS,
-    .TempCompensation = false,
-    .Driver      = &PIOS_HMC5x83_I2C_DRIVER,
-    .Orientation = PIOS_HMC5X83_ORIENTATION_EAST_NORTH_UP,
-};
-#endif /* PIOS_INCLUDE_HMC5X83 */
 
 /* One slot per selectable receiver group.
  *  eg. PWM, PPM, GCS, DSMMAINPORT, DSMFLEXIPORT, SBUS
@@ -263,13 +244,8 @@ void PIOS_Board_Init(void)
 
 #ifndef ERASE_FLASH
 #ifdef PIOS_INCLUDE_WDG
-    /* From TauLabs
-     * Initialize watchdog as early as possible to catch faults during init
-     * but do it only if there is no debugger connected
-     */
-    if ((CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk) == 0) {
-        PIOS_WDG_Init();
-    }
+    /* Initialize watchdog as early as possible to catch faults during init */
+    PIOS_WDG_Init();
 #endif
 #endif
 
@@ -742,25 +718,11 @@ void PIOS_Board_Init(void)
         break;
     case HWSETTINGS_CC_FLEXIPORT_I2C:
 #if defined(PIOS_INCLUDE_I2C)
-        if (PIOS_I2C_Init(&pios_i2c_flexiport_adapter_id, &pios_i2c_flexiport_adapter_cfg)) {
-            PIOS_Assert(0);
+        {
+            if (PIOS_I2C_Init(&pios_i2c_flexi_adapter_id, &pios_i2c_flexi_adapter_cfg)) {
+                PIOS_Assert(0);
+            }
         }
-        PIOS_DELAY_WaitmS(50);  // this was after the other PIOS_I2C_Init() on Revo, so I copied it here too
-#if defined(PIOS_INCLUDE_HMC5X83)
-        // get auxmag type
-        AuxMagSettingsTypeOptions option;
-        AuxMagSettingsInitialize();
-        AuxMagSettingsTypeGet(&option);
-        // if the aux mag type is FlexiPort then set it up
-        if (option == AUXMAGSETTINGS_TYPE_FLEXI) {
-            // attach the 5x83 mag to the previously inited I2C2
-            external_mag = PIOS_HMC5x83_Init(&pios_hmc5x83_external_cfg, pios_i2c_flexiport_adapter_id, 0);
-            // add this sensor to the sensor task's list
-            PIOS_HMC5x83_Register(external_mag, PIOS_SENSORS_TYPE_3AXIS_AUXMAG);
-            // mag alarm is cleared later, so use I2C
-            AlarmsSet(SYSTEMALARMS_ALARM_I2C, (external_mag)?SYSTEMALARMS_ALARM_OK:SYSTEMALARMS_ALARM_WARNING);
-        }
-#endif /* PIOS_INCLUDE_HMC5X83 */
 #endif /* PIOS_INCLUDE_I2C */
         break;
     case HWSETTINGS_CC_FLEXIPORT_OSDHK:

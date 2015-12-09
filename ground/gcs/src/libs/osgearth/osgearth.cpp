fabdea9eee0a079902c9a132c3601ce40e0e8eee
Philippe Renon <op.filnet@yahoo.fr>
2015-12-09 08:35:48 +0100

    LP-29 redirect osg logging to Qt (commented out for now)

--- ground/gcs/src/libs/osgearth/osgearth.cpp
+++ ground/gcs/src/libs/osgearth/osgearth.cpp
@@ -69,6 +69,9 @@ void OsgEarth::registerQmlTypes()
     }
     registered = true;
 
+    // redirect osg logging to Qt (use OSG_DEBUG environment variable to enable osg logging)
+    // osg::setNotifyHandler(new QtNotifyHandler());
+
     // initialize();
 
     // Register Qml types

ad84e51a0428bebf13f6d696617f56285af13e59
Philippe Renon <philippe_renon@yahoo.fr>
2017-03-10 17:29:53 +0100

    LP-491 upgrade 3rdparty build to osg 3.5.5 and osgearth 2.8
    from osg 3.5.3 and osgearth 2.7
    
    the osg upgrade allows removal of the Qt dependency
    this means that we don't need to recompile osg and
    osgearth for all OSes each time we upgrade Qt :)

--- make/3rdparty/osgearth/osgearth.mk
+++ make/3rdparty/osgearth/osgearth.mk
@@ -23,7 +23,7 @@
 OSG_BUILD_CONF := Release
 
 OSG_NAME_PREFIX :=
-OSG_NAME_SUFIX := -qt-$(QT_VERSION)
+OSG_NAME_SUFIX :=
 
 ################################
 #
@@ -31,7 +31,7 @@ OSG_NAME_SUFIX := -qt-$(QT_VERSION)
 #
 ################################
 
-OSG_VERSION := 3.5.3
+OSG_VERSION := 3.5.5
 OSG_GIT_TAG := OpenSceneGraph-$(OSG_VERSION)
 
 OSG_BASE_NAME   := osg-$(OSG_VERSION)
@@ -45,14 +45,11 @@ ifeq ($(UNAME), Linux)
 	OSG_CMAKE_GENERATOR := "Unix Makefiles"
 	OSG_CMAKE_MAKE_PROGRAM := make
 	OSG_WINDOWING_SYSTEM := "X11"
-	# for some reason Qt is not added to the path in make/tools.mk
-	OSG_BUILD_PATH := $(QT_SDK_PREFIX)/bin:$(PATH)
 else ifeq ($(UNAME), Darwin)
 	OSG_NAME := $(OSG_BASE_NAME)-clang_64
 	OSG_CMAKE_GENERATOR := "Unix Makefiles"
 	OSG_CMAKE_MAKE_PROGRAM := make
 	OSG_WINDOWING_SYSTEM := "Cocoa"
-	OSG_BUILD_PATH := $(QT_SDK_PREFIX)/bin:$(PATH)
 else ifeq ($(UNAME), Windows)
 	OSG_NAME := $(OSG_BASE_NAME)-$(QT_SDK_ARCH)
 	OSG_CMAKE_GENERATOR := "MinGW Makefiles"
@@ -62,7 +59,6 @@ OSG_NAME        := $(OSG_NAME_PREFIX)$(OSG_NAME)$(OSG_NAME_SUFIX)
 OSG_SRC_DIR     := $(ROOT_DIR)/3rdparty/osg
 OSG_BUILD_DIR   := $(BUILD_DIR)/3rdparty/$(OSG_NAME)
 OSG_INSTALL_DIR := $(BUILD_DIR)/3rdparty/install/$(OSG_NAME)
-OSG_PATCH_FILE  := $(ROOT_DIR)/make/3rdparty/osgearth/osg-$(OSG_VERSION).patch
 
 .PHONY: osg
 osg:
@@ -74,17 +70,15 @@ osg:
 		fi ; \
 		$(CMAKE) -G $(OSG_CMAKE_GENERATOR) -DCMAKE_BUILD_TYPE=$(OSG_BUILD_CONF) \
 			-DCMAKE_MAKE_PROGRAM=$(MAKE) \
-			-DOSG_USE_QT=ON \
 			-DBUILD_OSG_APPLICATIONS=ON \
 			-DBUILD_OSG_EXAMPLES=OFF \
-			-DBUILD_OPENTHREADS_WITH_QT=OFF \
 			-DOSG_GL3_AVAILABLE=OFF \
 			-DOSG_PLUGIN_SEARCH_INSTALL_DIR_FOR_PLUGINS=OFF \
 			-DCMAKE_OSX_ARCHITECTURES="x86_64" \
 			-DOSG_WINDOWING_SYSTEM=$(OSG_WINDOWING_SYSTEM) \
 			-DCMAKE_INSTALL_NAME_DIR=@executable_path/../Plugins \
 			-DCMAKE_INSTALL_PREFIX=$(OSG_INSTALL_DIR) $(OSG_SRC_DIR) && \
-		$(MAKE) && \
+		$(MAKE) -j3 && \
 		$(MAKE) install ; \
 	)
 
@@ -98,34 +92,6 @@ package_osg:
 		$(ZIP) -f $(notdir $(OSG_INSTALL_DIR)).tar && \
 		$(call MD5_GEN_TEMPLATE,$(notdir $(OSG_INSTALL_DIR)).tar.gz) ; \
 	)
-
-.PHONY: install_win_osg
-install_win_osg:
-	# curl
-	$(V1) $(CP) /mingw32/bin/libcurl-4.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libidn-11.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/librtmp-1.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libgmp-10.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libgnutls-30.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libp11-kit-0.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libffi-6.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libtasn1-6.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libhogweed-4-1.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libnettle-6-1.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libssh2-1.dll $(OSG_INSTALL_DIR)/bin/
-    # gdal
-	$(V1) $(CP) /mingw32/bin/libgdal-20.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libgeos_c.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libgeos.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libjpeg-8.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libtiff-5.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/liblzma-5.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libiconv-2.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/zlib1.dll $(OSG_INSTALL_DIR)/bin/
-    # other
-	$(V1) $(CP) /mingw32/bin/libproj-9.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libfreetype-6.dll $(OSG_INSTALL_DIR)/bin/
-	$(V1) $(CP) /mingw32/bin/libpng16-16.dll $(OSG_INSTALL_DIR)/bin/
 
 .NOTPARALLEL:
 .PHONY: prepare_osg
@@ -141,10 +107,7 @@ clone_osg:
 	$(V1) $(GIT) clone --depth 1 --no-checkout -b $(OSG_GIT_TAG) git://github.com/openscenegraph/osg.git $(OSG_SRC_DIR)
 	@$(ECHO) "Checkout osg $(OSG_GIT_TAG)"
 	$(V1) ( $(CD) $(OSG_SRC_DIR) && $(GIT) checkout --force tags/$(OSG_GIT_TAG) ; )
-	$(V1) if [ -e $(OSG_PATCH_FILE) ]; then \
-		$(ECHO) "Patching osg..." ; \
-		( $(CD) $(OSG_SRC_DIR) && $(GIT) apply $(OSG_PATCH_FILE) ; ) \
-	fi
+	$(V1) ( $(CD) $(OSG_SRC_DIR) && $(GIT) apply $(ROOT_DIR)/make/3rdparty/osgearth/osg-fix-3ds-plugin.patch ; )
 
 .PHONY: clean_osg
 clean_osg:
@@ -168,7 +131,7 @@ clean_all_osg: clean_osg
 # fix Debug build
 # add option to not build the applications (in Debug mode in particular)
 
-OSGEARTH_VERSION := 2.7
+OSGEARTH_VERSION := 2.8
 OSGEARTH_GIT_TAG := osgearth-$(OSGEARTH_VERSION)
 
 OSGEARTH_BASE_NAME   := osgearth-$(OSGEARTH_VERSION)
@@ -184,8 +147,7 @@ ifeq ($(UNAME), Linux)
 	endif
 	OSGEARTH_CMAKE_GENERATOR := "Unix Makefiles"
 	OSGEARTH_CMAKE_MAKE_PROGRAM := make
-	# for some reason Qt is not added to the path in make/tools.mk
-	OSGEARTH_BUILD_PATH := $(QT_SDK_PREFIX)/bin:$(OSG_INSTALL_DIR)/bin:$(PATH)
+	OSGEARTH_BUILD_PATH := $(OSG_INSTALL_DIR)/bin:$(PATH)
 	ifeq ($(ARCH), x86_64)
 		OSGEARTH_LIB_PATH := $(OSG_INSTALL_DIR)/lib64
 	else
@@ -195,7 +157,7 @@ else ifeq ($(UNAME), Darwin)
 	OSGEARTH_NAME := $(OSGEARTH_BASE_NAME)-clang_64
 	OSGEARTH_CMAKE_GENERATOR := "Unix Makefiles"
 	OSGEARTH_CMAKE_MAKE_PROGRAM := make
-	OSGEARTH_BUILD_PATH := $(QT_SDK_PREFIX)/bin:$(OSG_INSTALL_DIR)/bin:$(PATH)
+	OSGEARTH_BUILD_PATH := $(OSG_INSTALL_DIR)/bin:$(PATH)
 	OSGEARTH_LIB_PATH := $(OSG_INSTALL_DIR)/lib
 else ifeq ($(UNAME), Windows)
 	OSGEARTH_NAME := $(OSGEARTH_BASE_NAME)-$(QT_SDK_ARCH)
@@ -207,7 +169,6 @@ OSGEARTH_NAME        := $(OSG_NAME_PREFIX)$(OSGEARTH_NAME)$(OSG_NAME_SUFIX)
 OSGEARTH_SRC_DIR     := $(ROOT_DIR)/3rdparty/osgearth
 OSGEARTH_BUILD_DIR   := $(BUILD_DIR)/3rdparty/$(OSGEARTH_NAME)
 OSGEARTH_INSTALL_DIR := $(BUILD_DIR)/3rdparty/install/$(OSGEARTH_NAME)
-OSGEARTH_PATCH_FILE  := $(ROOT_DIR)/make/3rdparty/osgearth/osgearth-$(OSGEARTH_VERSION).patch
 
 .PHONY: osgearth
 osgearth:
@@ -222,7 +183,7 @@ osgearth:
 		unset OSG_NOTIFY_LEVEL && \
 		$(CMAKE) -G $(OSGEARTH_CMAKE_GENERATOR) -DCMAKE_BUILD_TYPE=$(OSGEARTH_BUILD_CONF) \
 			-DCMAKE_MAKE_PROGRAM=$(MAKE) \
-			-DOSGEARTH_USE_QT=ON \
+			-DOSGEARTH_USE_QT=OFF \
 			-DINSTALL_TO_OSG_DIR=OFF \
 			-DOSG_DIR=$(OSG_INSTALL_DIR) \
 			-DCMAKE_INCLUDE_PATH=$(OSG_INSTALL_DIR)/include \
@@ -231,7 +192,7 @@ osgearth:
 			-DCMAKE_OSX_ARCHITECTURES="x86_64" \
 			-DCMAKE_INSTALL_NAME_DIR=@executable_path/../Plugins \
 			-DCMAKE_INSTALL_PREFIX=$(OSGEARTH_INSTALL_DIR) $(OSGEARTH_SRC_DIR) && \
-		$(MAKE) && \
+		$(MAKE) -j3 && \
 		$(MAKE) install ; \
 	)
 
@@ -259,10 +220,8 @@ clone_osgearth:
 	$(V1) $(GIT) clone --depth 1 --no-checkout -b $(OSGEARTH_GIT_TAG) git://github.com/gwaldron/osgearth.git $(OSGEARTH_SRC_DIR)
 	@$(ECHO) "Checkout osgearth $(OSGEARTH_GIT_TAG)"
 	$(V1) ( $(CD) $(OSGEARTH_SRC_DIR) && $(GIT) checkout --force tags/$(OSGEARTH_GIT_TAG) ; )
-	$(V1) if [ -e $(OSGEARTH_PATCH_FILE) ]; then \
-		$(ECHO) "Patching osgearth..." ; \
-		( $(CD) $(OSGEARTH_SRC_DIR) && $(GIT) apply $(OSGEARTH_PATCH_FILE) ; ) \
-	fi
+	$(V1) ( $(CD) $(OSGEARTH_SRC_DIR) && $(GIT) apply $(ROOT_DIR)/make/3rdparty/osgearth/osgearth-remove-deprecated-call.patch ; )
+	$(V1) ( $(CD) $(OSGEARTH_SRC_DIR) && $(GIT) apply $(ROOT_DIR)/make/3rdparty/osgearth/osgearth-geos-3_6_1-support.patch ; )
 
 .PHONY: clean_osgearth
 clean_osgearth:
@@ -284,9 +243,4 @@ clean_all_osgearth: clean_osgearth
 
 .NOTPARALLEL:
 .PHONY: all_osg
-
-ifeq ($(UNAME), Windows)
-all_osg: prepare_osg prepare_osgearth osg osgearth install_win_osg package_osg package_osgearth
-else
 all_osg: prepare_osg prepare_osgearth osg osgearth package_osg package_osgearth
-endif

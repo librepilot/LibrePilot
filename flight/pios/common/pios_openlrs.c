131a6b0a34376fe4ee531a19e91d1718fdaeb84f
Laurent Lalanne <f5soh@free.fr>
2018-03-13 00:30:16 +0100

    LP-585 Uncomment/fix openlrs debug

--- flight/pios/common/pios_openlrs.c
+++ flight/pios/common/pios_openlrs.c
@@ -1276,13 +1276,13 @@ static void pios_openlrs_task(void *parameters)
                 }
             } else {
                 // We timed out because packet was missed
-                DEBUG_PRINTF(3, "ISR Timeout. Missed packet: %d %d %d\r\n", delay, getInterval(&openlrs_dev->bind_data), time_since_packet_us);
+                // DEBUG_PRINTF(3, "ISR Timeout. Missed packet: %d %d %d\r\n", delay_ms, getInterval(&openlrs_dev->bind_data), time_since_packet_us);
                 pios_openlrs_rx_loop(openlrs_dev);
             }
 
             rssi_sampled = true;
         } else {
-            // DEBUG_PRINTF(3, "ISR %d %d %d\r\n", delay, getInterval(&openlrs_dev->bind_data), time_since_packet_us);
+            // DEBUG_PRINTF(3, "ISR %d %d %d\r\n", delay_ms, getInterval(&openlrs_dev->bind_data), time_since_packet_us);
 
             // Process incoming data
             pios_openlrs_rx_loop(openlrs_dev);
